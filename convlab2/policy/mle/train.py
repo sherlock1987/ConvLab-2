fa8c5685b734290090e09985710d749f9f77a6ab
sherlock1987 <holmes_hzx@163.com>
2020-06-01 20:35:53 +0800

    converge on real data, not SU, and converge around 15, litte better than SU, but not so much. Good luck, boy

--- convlab2/policy/mle/train.py
+++ convlab2/policy/mle/train.py
@@ -2,17 +2,45 @@ import os
 import torch
 import logging
 import torch.nn as nn
-
+import numpy as np
 from convlab2.util.train_util import to_device
-
+import torch.nn as nn
+from torch import optim
+import zipfile
+import sys
+import matplotlib.pyplot  as plt
+import pickle
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import torch.tensor as tensor
+class Reward_predict(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Reward_predict, self).__init__()
+        self.encoder_1 = nn.LSTM(input_size, output_size, batch_first=True, bidirectional=False)
+        self.encoder_2 = nn.LSTM(output_size, output_size)
+
+        self.m = nn.Sigmoid()
+        self.loss = nn.BCELoss(size_average= False,reduce= True)
+        self.cnn_belief = nn.Linear(input_size-output_size,output_size)
+        self.cnn_output = nn.Linear(output_size,output_size)
+    def forward(self, input_feature, input_belief, target):
+        # to construct the batch first, then we could compute the loss function for this stuff, simple and easy.
+        _, (last_hidden, last_cell) = self.encoder_1(input_feature)
+        # second Part
+
+        _, (predict_action, last_cell) = self.encoder_2(self.cnn_belief(input_belief), (last_hidden, last_cell))
+
+        loss = self.loss(self.m(self.cnn_output(predict_action)),target)
+        return loss
 
 class MLE_Trainer_Abstract():
     def __init__(self, manager, cfg):
         self._init_data(manager, cfg)
         self.policy = None
         self.policy_optim = None
-        
+
+        # define the stuff from the reward machine
+
+
     def _init_data(self, manager, cfg):
         self.data_train = manager.create_dataset('train', cfg['batchsz'])
         self.data_valid = manager.create_dataset('val', cfg['batchsz'])
@@ -21,21 +49,90 @@ class MLE_Trainer_Abstract():
         self.print_per_batch = cfg['print_per_batch']
         self.save_per_epoch = cfg['save_per_epoch']
         self.multi_entropy_loss = nn.MultiLabelSoftMarginLoss()
-        
+        self.loss_record = []
+        self.reward_predictor = Reward_predict(549, 457, 209)
+        self.reward_optim = optim.Adam(self.reward_predictor.parameters(), lr=1e-4)
+    #    init the terminate state and use if when training our model.
+        self.terminate_train = {}
+        for part in ['train', 'val', 'test']:
+            with open(os.path.join("//home//raliegh//图片//ConvLab-2//convlab2//policy//mle//multiwoz//processed_data", '{}_terminate.pkl'.format(part)), 'rb') as f:
+                self.terminate_train[part] = pickle.load(f)
+
     def policy_loop(self, data):
         s, target_a = to_device(data)
         a_weights = self.policy(s)
-        
+
         loss_a = self.multi_entropy_loss(a_weights, target_a)
         return loss_a
-        
+
+    def reward_training(self,epoch):
+        self.reward_predictor.train()
+        s_temp = torch.tensor([])
+        a_temp = torch.tensor([])
+        loss = torch.tensor([0]).float()
+
+        for i,data in enumerate(self.data_train):
+            s, a = to_device(data)
+            # s_temp = s[:i+1]
+            try:
+                s_temp = torch.cat((s_temp,s),0)
+                a_temp = torch.cat((a_temp,a),0)
+            except Exception as e:
+                s_temp = s
+                a_temp = a
+
+            s_train = s_temp.unsqueeze(0)
+            a_train = a_temp.unsqueeze(0)
+            # print(s_train.shape)
+            if len(s_train[0])>=2:
+                # print("-"*300)
+                input_pre = torch.cat((s_train,a_train),2)[0][:-1].unsqueeze(0)
+                input_bf = s_train[0][-1].unsqueeze(0).unsqueeze(0)
+                target = a_train[0][-1].unsqueeze(0).unsqueeze(0)
+                # print(input_pre.shape,input_bf.shape,target.shape)
+
+                terminate = self.terminate_train["train"][i]
+                if terminate == False:
+                    micro_loss = self.reward_predictor(input_pre, input_bf, target)
+                    loss += micro_loss
+                else:
+                    # predict the last one and then loss backward and then clear the button
+                    micro_loss = self.reward_predictor(input_pre, input_bf, target)
+                    loss += micro_loss
+                    # print(loss,loss/(len(s_temp)-3))
+                    if loss != torch.tensor([0]).float():
+                        # print(loss.item()/(len(iteration)-3),loss.shape)
+                        loss.backward()
+                        for name, param in self.reward_predictor.named_parameters():
+                            if "cnn" not in name:
+                                # print(name)
+                                # print(param.grad)
+                                pass
+                        self.reward_optim.step()
+                        self.reward_optim.zero_grad()
+                        self.loss_record.append(loss.item())
+                        # clear the button
+                        s_temp = torch.tensor([])
+                        a_temp = torch.tensor([])
+                        loss = torch.tensor([0]).float()
+    #         remember to save the model
+
+        if (epoch + 1) % self.save_per_epoch == 0:
+            self.reward_model_save(self.save_dir, epoch)
+            axis = [i for i in range(len(self.loss_record))]
+            plt.plot(axis, self.loss_record)
+            plt.xlabel('Number of turns')
+            plt.ylabel('Embedding Loss')
+            plt.show()
     def imitating(self, epoch):
         """
         pretrain the policy by simple imitation learning (behavioral cloning)
         """
         self.policy.train()
         a_loss = 0.
+        count = 0
         for i, data in enumerate(self.data_train):
+            terminate = self.terminate_train["train"][i]
             self.policy_optim.zero_grad()
             loss_a = self.policy_loop(data)
             a_loss += loss_a.item()
@@ -114,3 +211,10 @@ class MLE_Trainer_Abstract():
 
         logging.info('<<dialog policy>> epoch {}: saved network to mdl'.format(epoch))
 
+    def reward_model_save(self, directory, epoch):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        torch.save(self.policy.state_dict(), directory + '/' + str(epoch) + 'reward_mle.pol.mdl')
+
+        logging.info('<<dialog policy>> epoch {}: saved network to mdl'.format(epoch))

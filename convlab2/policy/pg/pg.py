4c74832b79450e04df3bfe535d5c595b5ada0e93
sherlock1987 <holmes_hzx@163.com>
2020-06-03 11:18:40 +0800

    add the reward func to pg, and find this one is a little better in the performance, but still don't know how is this one look like in training process

--- convlab2/policy/pg/pg.py
+++ convlab2/policy/pg/pg.py
@@ -22,26 +22,34 @@ sys.path.append(root_dir)
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class Reward_predict(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Reward_predict, self).__init__()
+        self.encoder_1 = nn.LSTM(input_size, output_size, batch_first=True, bidirectional=False)
+        self.encoder_2 = nn.LSTM(output_size, output_size)
 
-    def __init__(self,input_size, hidden_size):
-        super(Reward_predict,self).__init__()
-        self.cnn = nn.Linear(input_size, hidden_size, bias=True)
-        self.encoder = nn.LSTM(hidden_size,hidden_size,batch_first=True,bidirectional=False)
-        self.cnn2 = nn.Linear(hidden_size,hidden_size,bias=True)
-        self.loss = nn.CosineEmbeddingLoss()
-
-    def forward(self, input,target):
+        self.m = nn.Sigmoid()
+        self.loss = nn.BCELoss(size_average= False,reduce= True)
+        self.cnn_belief = nn.Linear(input_size-output_size,output_size)
+        self.cnn_output = nn.Linear(output_size,output_size)
+    def forward(self, input_feature, input_belief, target):
         # to construct the batch first, then we could compute the loss function for this stuff, simple and easy.
-        feature_input = self.cnn(input)
-        _, (predictor_vec, last_cell) = self.encoder(feature_input)
-        loss = self.loss(predictor_vec,target,target=torch.tensor(1))
+        _, (last_hidden, last_cell) = self.encoder_1(input_feature)
+        # second Part
+
+        _, (predict_action, last_cell) = self.encoder_2(self.cnn_belief(input_belief), (last_hidden, last_cell))
+
+        loss = self.loss(self.m(self.cnn_output(predict_action)),target)
         return loss
 
-    def target_extract(self,target):
-        with torch.no_grad():
-            feature_target = self.cnn(target)
-            _, (encoded_target, last_cell) = self.encoder(feature_target)
-            return encoded_target
+    def compute_reward(self, input_feature,input_belief,input_predict_RL):
+        # compute the reward based on the very easy methoc
+        _, (last_hidden, last_cell) = self.encoder_1(input_feature)
+        # second Part
+        _, (predict_action, last_cell) = self.encoder_2(self.cnn_belief(input_belief), (last_hidden, last_cell))
+        action_prob = self.m(self.cnn_output(predict_action))
+        reward = action_prob.unsqueeze(0) * input_predict_RL.unsqueeze(0)
+        res = torch.sum(reward.unsqueeze(0).unsqueeze(0))
+        return res
 
 class PG(Policy):
     def __init__(self, is_train=False, dataset='Multiwoz'):
@@ -67,8 +75,8 @@ class PG(Policy):
             self.policy_optim = optim.RMSprop(self.policy.parameters(), lr=cfg['lr'])
 
         # define the predictor params
-        self.predictor_reward = Reward_predict(549,457)
-        self.optim_reward = optim.Adam(self.predictor_reward.parameters(), lr=1e-1 )
+        self.reward_predictor = Reward_predict(549,457,209)
+        self.reward_optim = optim.Adam(self.reward_predictor.parameters(), lr=1e-4 )
         self.loss_record = []
 
     def predict(self, state):
@@ -111,16 +119,65 @@ class PG(Policy):
             # mask = 0 means the immediate reward is the real V(s) since it's end of trajectory.
             # formula: V(s_t) = r_t + gamma * V(s_t+1)
             v_target[t] = r[t] + self.gamma * prev_v_target * mask[t]
-
             # update previous
             prev_v_target = v_target[t]
 
         return v_target
 
+    def sparse_reward(self,r,mask):
+        return r
+    def reward_predict(self, s, a, r, mask):
+        reward_predict = []
+        batchsz = r.shape[0]
+        s_temp = torch.tensor([])
+        a_temp = torch.tensor([])
+        for i in range(batchsz):
+            # current　states and actions
+            s_1 = s[i].unsqueeze(0)
+            a_1 = a[i].unsqueeze(0)
+            try:
+                s_temp = torch.cat((s_temp,s_1),0)
+                a_temp = torch.cat((a_temp,a_1),0)
+            except Exception as e:
+                s_temp = s_1
+                a_temp = a_1
+            s_train = s_temp.unsqueeze(0).float()
+            a_train = a_temp.unsqueeze(0).float()
+
+            input = torch.cat((s_train, a_train), 2)
+            # input_bf = s_train[-1].unsqueeze(0)
+            # target = a_train[-1].unsqueeze(0)
+            # 长度大于2 的话呢.
+
+            if len(input[0]) >=2:
+                input_pre = input.squeeze(0)[:-1].unsqueeze(0)
+                input_bf = s_train.squeeze(0)[-1].unsqueeze(0).unsqueeze(0)
+                target = a_train.squeeze(0)[-1].unsqueeze(0).unsqueeze(0)
+                # print(input_pre.shape,input_bf.shape,target.shape)
+                if int(mask[i]) == 0:
+                    # cur_reward = self.reward_predictor.compute_reward(input_pre, input_bf, target)
+                    cur_reward = r[i]
+                    s_temp = torch.tensor([])
+                    a_temp = torch.tensor([])
+                #   compute the last one, terminate clear the button, that is okay for us.
+                else:
+                    cur_reward = self.reward_predictor.compute_reward(input_pre, input_bf, target)
+                reward_predict.append(cur_reward.item())
+            else:
+    #             when the lengh is 1, the start reward is 1
+                reward_predict.append(1)
+
+        # add the bellman equation
+        # reward_bell_man = self.est_return(tensor(reward_predict),mask)
+
+        return tensor(reward_predict)
+
     def update(self, epoch, batchsz, s, a, r, mask):
         # generate a sequence of this one, it is fine for me
-        v_target = self.est_return(r, mask)
-
+        # change this place
+        # v_target = self.est_return(r, mask)
+        v_target = (self.  (s,a,r,mask))
+        # v_target = self.sparse_reward(r,mask)
         # from mask to do the divide and get the index
         batch_index = []
         temp = []
@@ -131,76 +188,76 @@ class PG(Policy):
                 temp = []
             else:
                 temp.append(index)
-
         for i in range(self.update_round):
-            for iteration in batch_index:
-                loss = torch.tensor([0]).float()
-                # starting looping and make the sum of loss.
-                print("-" * 30,len(iteration))
-                for index, _ in enumerate(iteration):
-                    temp = iteration[:index + 1]
-                    if len(temp) >= 4:
-                        # start training
-                        s_b = s[temp].float()
-                        a_b = a[temp].float()
-                        input = torch.cat((s_b[:-1], a_b[:-1]), -1).unsqueeze(0)
-                        target = torch.cat((s_b[-1], a_b[-1]), -1).unsqueeze(0).unsqueeze(0)
-                        target = self.predictor_reward.target_extract(target)
+            # for iteration in batch_index:
+            #     loss = torch.tensor([0]).float()
+            #     # starting looping and make the sum of loss.
+            #     print("-" * 30,len(iteration))
+            #     print(s[iteration[0]])
+            #     for index, _ in enumerate(iteration):
+            #         temp = iteration[:index+1]
+            #         if len(temp) >= 4:
+            #             # start training
+            #             s_b = s[temp].float()
+            #             a_b = a[temp].float()
+            #             input = torch.cat((s_b[:-1], a_b[:-1]), -1).unsqueeze(0)
+            #             belief = s_b[-1].unsqueeze(0).unsqueeze(0)
+            #             target = a_b[-1].unsqueeze(0).unsqueeze(0)
+            #
+            #             micro_loss = self.predictor_reward(input, belief,target)
+            #             # print(micro_loss)
+            #             loss += micro_loss
+            #     # backwarding...
+            #     if loss != torch.tensor([0]).float():
+            #         # print(loss.item()/(len(iteration)-3),loss.shape)
+            #         loss.backward()
+            #         for name,param in self.predictor_reward.named_parameters():
+            #             if "cnn" not in name:
+            #                 # print(name)
+            #                 # print(param.grad)
+            #                 pass
+            #         self.optim_reward.step()
+            #         self.predictor_reward.zero_grad()
+            #         self.loss_record.append(loss.item()/len(iteration))
+            # 1. shuffle current batch
+            perm = torch.randperm(batchsz)
+            # shuffle the variable for mutliple optimize
+            # v_target_shuf, s_shuf, a_shuf = v_target[perm], s[perm], a[perm]
+            v_target_shuf, s_shuf, a_shuf = v_target, s, a
+            # 2. get mini-batch for optimizing
+            optim_chunk_num = int(np.ceil(batchsz / self.optim_batchsz))
+            # chunk the optim_batch for total batch
+            v_target_shuf, s_shuf, a_shuf = torch.chunk(v_target_shuf, optim_chunk_num), \
+                                            torch.chunk(s_shuf, optim_chunk_num), \
+                                            torch.chunk(a_shuf, optim_chunk_num)
 
-                        micro_loss = self.predictor_reward(input, target)
-                        # print(micro_loss)
-                        loss += micro_loss
-                # backwarding...
-                if loss != torch.tensor([0]).float():
-                    print(loss,loss.shape)
-                    loss.backward()
-                    for name,param in self.predictor_reward.named_parameters():
-                        if "cnn" not in name:
-                            # print(name)
-                            # print(param.grad)
-                            pass
-                    self.optim_reward.step()
-                    self.predictor_reward.zero_grad()
-                    self.loss_record.append(loss.item())
-            # # 1. shuffle current batch
-            # perm = torch.randperm(batchsz)
-            # # shuffle the variable for mutliple optimize
-            # # v_target_shuf, s_shuf, a_shuf = v_target[perm], s[perm], a[perm]
-            # v_target_shuf, s_shuf, a_shuf = v_target, s, a
-            # # 2. get mini-batch for optimizing
-            # optim_chunk_num = int(np.ceil(batchsz / self.optim_batchsz))
-            # # chunk the optim_batch for total batch
-            # v_target_shuf, s_shuf, a_shuf = torch.chunk(v_target_shuf, optim_chunk_num), \
-            #                                 torch.chunk(s_shuf, optim_chunk_num), \
-            #                                 torch.chunk(a_shuf, optim_chunk_num)
-            #
-            # # 3. iterate all mini-batch to optimize
-            # policy_loss = 0.
-            # for v_target_b, s_b, a_b in zip(v_target_shuf, s_shuf, a_shuf):
-            #     # print('optim:', batchsz, v_target_b.size(), A_sa_b.size(), s_b.size(), a_b.size(), log_pi_old_sa_b.size())
-            #
-            #     # update policy network by clipping
-            #     self.policy_optim.zero_grad()
-            #     # [b, 1]
-            #     log_pi_sa = self.policy.get_log_prob(s_b, a_b)
-            #     # ratio = exp(log_Pi(a|s) - log_Pi_old(a|s)) = Pi(a|s) / Pi_old(a|s)
-            #     # we use log_pi for stability of numerical operation
-            #     # [b, 1] => [b]
-            #     # this is element-wise comparing.
-            #     # we add negative symbol to convert gradient ascent to gradient descent
-            #     surrogate = - (log_pi_sa * v_target_b).mean()
-            #     policy_loss += surrogate.item()
-            #
-            #     # backprop
-            #     surrogate.backward()
-            #     # gradient clipping, for stability
-            #     torch.nn.utils.clip_grad_norm(self.policy.parameters(), 10)
-            #     # self.lock.acquire() # retain lock to update weights
-            #     self.policy_optim.step()
-            #     # self.lock.release() # release lock
-            #
-            # policy_loss /= optim_chunk_num
-            # logging.debug('<<dialog policy pg>> epoch {}, iteration {}, policy, loss {}'.format(epoch, i, policy_loss))
+            # 3. iterate all mini-batch to optimize
+            policy_loss = 0.
+            for v_target_b, s_b, a_b in zip(v_target_shuf, s_shuf, a_shuf):
+                # print('optim:', batchsz, v_target_b.size(), A_sa_b.size(), s_b.size(), a_b.size(), log_pi_old_sa_b.size())
+
+                # update policy network by clipping
+                self.policy_optim.zero_grad()
+                # [b, 1]
+                log_pi_sa = self.policy.get_log_prob(s_b, a_b)
+                # ratio = exp(log_Pi(a|s) - log_Pi_old(a|s)) = Pi(a|s) / Pi_old(a|s)
+                # we use log_pi for stability of numerical operation
+                # [b, 1] => [b]
+                # this is element-wise comparing.
+                # we add negative symbol to convert gradient ascent to gradient descent
+                surrogate = - (log_pi_sa * v_target_b).mean()
+                policy_loss += surrogate.item()
+
+                # backprop
+                surrogate.backward()
+                # gradient clipping, for stability
+                torch.nn.utils.clip_grad_norm(self.policy.parameters(), 10)
+                # self.lock.acquire() # retain lock to update weights
+                self.policy_optim.step()
+                # self.lock.release() # release lock
+
+            policy_loss /= optim_chunk_num
+            logging.debug('<<dialog policy pg>> epoch {}, iteration {}, policy, loss {}'.format(epoch, i, policy_loss))
 
         if (epoch + 1) % self.save_per_epoch == 0:
             self.save(self.save_dir, epoch)
@@ -209,15 +266,16 @@ class PG(Policy):
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        # torch.save(self.policy.state_dict(), directory + '/' + str(epoch) + '_pg.pol.mdl')
-        torch.save(self.predictor_reward.state_dict(), directory + '/' + str(epoch) + '_pg.pol.mdl')
+        torch.save(self.policy.state_dict(), directory + '/' + str(epoch) + '_pg_plus_reward.pol.mdl')
+        # torch.save(self.reward_predictor.state_dict(), directory + '/' + str(epoch) + '_pg_only_reward.pol.mdl')
 
         logging.info('<<dialog policy>> epoch {}: saved network to mdl'.format(epoch))
-        axis = [i for i in range(len(self.loss_record))]
-        plt.plot(axis,self.loss_record)
-        plt.xlabel('Number of dialogue turns')
-        plt.ylabel('Embedding Loss')
-        plt.show()
+        # draw the graph
+        # axis = [i for i in range(len(self.loss_record))]
+        # plt.plot(axis,self.loss_record)
+        # plt.xlabel('Number of dialogue turns')
+        # plt.ylabel('Embedding Loss')
+        # plt.show()
 
     def load(self, filename):
         policy_mdl_candidates = [
@@ -232,6 +290,22 @@ class PG(Policy):
             if os.path.exists(policy_mdl):
                 self.policy.load_state_dict(torch.load(policy_mdl, map_location=DEVICE))
                 logging.info('<<dialog policy>> loaded checkpoint from file: {}'.format(policy_mdl))
+                break
+
+    def load_reward_model(self, filename):
+        policy_mdl_candidates = [
+            filename,
+            filename + '.pol.mdl',
+            filename + '_pg.pol.mdl',
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), filename),
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), filename + '.pol.mdl'),
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), filename + '_pg.pol.mdl')
+        ]
+        for policy_mdl in policy_mdl_candidates:
+            if os.path.exists(policy_mdl):
+                network = torch.load(policy_mdl)
+                self.reward_predictor.load_state_dict(torch.load(policy_mdl, map_location=DEVICE))
+                logging.info('<<dialog policy>> loaded reward model checkpoint from file: {}'.format(policy_mdl))
                 break
 
     def load_from_pretrained(self, archive_file, model_file, filename):

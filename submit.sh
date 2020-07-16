9874eb34f1fa0863ab30f5a8452baec48b1f3801
sherlock1987 <holmes_hzx@163.com>
2020-07-16 12:00:24 +0800

    1_test_log(r(d))

--- submit.sh
+++ submit.sh
@@ -9,7 +9,6 @@ root=`pwd`
 echo "${MYDIR}"
 echo "${time}"
 
-
 config_path=${root}/"convlab2/policy/ppo/idea4/config.json"
 RL_path=${root}/"convlab2/policy/ppo/train.py"
 load_path=${root}/"convlab2/policy/mle/multiwoz/best_mle"
@@ -40,7 +39,7 @@ do
 	"load": "save/best"
 }
 '> ${config_path}
-  python ${RL_path} --load_path ${load_path} --load_reward_path ${root}/convlab2/policy/mle/idea4/GAN1/Dis/pretrain_D.mdl
+  python ${RL_path} --load_path ${load_path} --load_path_reward ${root}/convlab2/policy/mle/idea4/GAN1/Dis/pretrain_D.mdl
   echo "${log_path}"
   python ${Eval_path} --model_name "PPO" --evluate_in_dir True --model_path_root ${complete_sub_save_path} --log_res_path ${log_path}
   }&
